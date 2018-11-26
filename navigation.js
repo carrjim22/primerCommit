d3ef94261bc8b4ff95fe1c8c8223b2bb1bef8d07
Carrji22 <carrjim22@gmail.com>
2018-11-26 15:38:15 -0600

    he agregado un nuevo archivo y añadí el h1 en el index.html

---
+++ navigation.js
@@ -0,0 +1,5 @@
+const h1 = document.createElement('h1');
+ h1.setAttribute('id', 'titulo-navegacion');
+ h1.textContent = 'Aprendiendo Git';
+ h1.style.textAlign = 'center'
+ document.getElementById('nav').appendChild(h1)
